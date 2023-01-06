d4adfc01fe97b3fb1c0fbe5a1e7aa27acde4c3ff
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2023-01-07 01:08:29 +0200

    Rework XScreen

--- kvex/widgets/screen.py
+++ kvex/widgets/screen.py
@@ -1,68 +1,37 @@
 """Home of `ScreenManager` and `XScreen`."""
 
+from typing import Literal
 from .. import kivy as kv
 from .widget import XWidget
 
 
 class XScreen(XWidget, kv.Screen):
-    """Screen that can only contain one widget."""
-
-    def __init__(self, **kwargs):
-        """Same arguments as Screen."""
-        super().__init__(**kwargs)
-        self.view = None
-        """Widget of the screen."""
-
-    def add_widget(self, w, *args, **kwargs):
-        """Overrides base method to set the view."""
-        self.view = w
-        super().add_widget(w, *args, **kwargs)
-        if len(self.children) > 1:
-            raise RuntimeError(
-                f"Cannot add more than 1 widget to XScreen: {self.children=}"
-            )
+    """Screen."""
 
 
 class XScreenManager(XWidget, kv.ScreenManager):
     """ScreenManager with custom transition behavior."""
 
-    transition_speed = kv.NumericProperty(0.4)
-    """Speed of transitions in seconds."""
-
-    def __init__(self, **kwargs):
-        """Same arguments as for ScreenManager, minus transition."""
-        if "transition" in kwargs:
-            del kwargs["transition"]
-        super().__init__(**kwargs)
-        self.transition = kv.SlideTransition(
-            direction="left",
-            duration=self.transition_speed,
-        )
-        """Transition object."""
-
     def add_screen(self, name: str, widget: kv.Widget) -> XScreen:
         """Create and add a screen from a widget."""
-        screen = self.add_widget(XScreen(name=name))
+        screen = XScreen(name=name)
         screen.add_widget(widget)
+        self.add_widget(screen)
         return screen
 
-    def switch_name(self, name: str) -> bool:
-        """Switch to a screen by name."""
-        if name == self.current:
-            return True
-        if self.mid_transition:
-            return False
-        if name not in self.screen_names:
-            raise ValueError(f'Found no screen by name "{name}" in {self.screen_names}')
+    def screen_direction(self, name: str, /) -> Literal["left", "right"]:
+        """Compare index of a screen by name to the current screen.
+
+        Useful for setting transition direction:
+        ```python3
+        sm = XScreenManager()
+        sm.transition.direction = sm.screen_direction("screen_name")
+        sm.current = "screen_name"
+        ```
+        """
         old_index = self.screen_names.index(self.current)
         new_index = self.screen_names.index(name)
-        dir = "left" if old_index < new_index else "right"
-        self.transition = kv.SlideTransition(
-            direction=dir,
-            duration=self.transition_speed,
-        )
-        self.current = name
-        return True
+        return "left" if old_index < new_index else "right"
 
     @property
     def mid_transition(self) -> bool:
