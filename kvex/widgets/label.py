911eea82c360853ceeb3a3a06b08faf42e882784
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2023-01-14 13:26:54 +0200

    Add fixed height

--- kvex/widgets/label.py
+++ kvex/widgets/label.py
@@ -7,23 +7,32 @@ from .widget import XWidget, XThemed
 class XLabel(XThemed, XWidget, kv.Label):
     """Label."""
 
-    def __init__(self, fixed_width: bool = False, **kwargs):
+    def __init__(self, fixed_width: bool = False, fixed_height: bool = False, **kwargs):
         """Initialize the class.
 
         Args:
             fixed_width: Adjust the height of the label while maintaining width.
+            fixed_height: Adjust the width of the label while maintaining height.
         """
         kwargs = {
             "markup": True,
             "halign": "center",
             "valign": "center",
         } | kwargs
+        if fixed_width and fixed_height:
+            raise RuntimeError("Must set either fixed_width or fixed_height.")
         super().__init__(**kwargs)
         self._trigger_fix_height = kv.Clock.create_trigger(self._fix_height)
+        self._trigger_fix_width = kv.Clock.create_trigger(self._fix_width)
         if fixed_width:
             self.bind(
                 size=self._trigger_fix_height,
                 text=self._trigger_fix_height,
+            )
+        elif fixed_height:
+            self.bind(
+                size=self._trigger_fix_width,
+                text=self._trigger_fix_width,
             )
         else:
             self.bind(size=self._on_size)
@@ -37,6 +46,16 @@ class XLabel(XThemed, XWidget, kv.Label):
             self.set_size(x=x, y=self.texture_size[1])
         else:
             self.set_size(hx=hx, y=self.texture_size[1])
+
+    def _fix_width(self, *a):
+        y = self.size[1]
+        hy = self.size_hint[1]
+        self.text_size = None, y
+        self.texture_update()
+        if hy is None:
+            self.set_size(x=self.texture_size[0], y=y)
+        else:
+            self.set_size(x=self.texture_size[0], hy=hy)
 
     def _on_size(self, *a):
         self.text_size = self.size
